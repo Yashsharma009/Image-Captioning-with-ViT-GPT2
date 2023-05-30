43a806d1eb2997055a8824ab2e46ebca27e4a4d2
Yashsharma009 <116294789+Yashsharma009@users.noreply.github.com>
2023-05-30 12:23:28 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,39 +1,3 @@
-import torch
-from PIL import Image
-import streamlit as st
-from transformers import ViTFeatureExtractor, ViTForImageCaptioning, AutoTokenizer
-
-model_name = "nlpconnect/vit-gpt2-image-captioning"
-feature_extractor = ViTFeatureExtractor.from_pretrained(model_name)
-tokenizer = AutoTokenizer.from_pretrained(model_name)
-model = ViTForImageCaptioning.from_pretrained(model_name)
-
-device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
-model.to(device)
-
-max_length = 16
-num_beams = 4
-gen_kwargs = {"max_length": max_length, "num_beams": num_beams}
-
-
-def predict_step(image_paths):
-    images = []
-    for image_path in image_paths:
-        i_image = Image.open(image_path)
-        if i_image.mode != "RGB":
-            i_image = i_image.convert(mode="RGB")
-        images.append(i_image)
-
-    pixel_values = feature_extractor(images=images, return_tensors="pt").pixel_values
-    pixel_values = pixel_values.to(device)
-
-    output_ids = model.generate(pixel_values, **gen_kwargs)
-
-    preds = tokenizer.batch_decode(output_ids, skip_special_tokens=True)
-    preds = [pred.strip() for pred in preds]
-    return preds
-
-
 def main():
     st.title("Image Captioning with ViT-GPT2")
     st.write("Upload an image and get a caption!")
@@ -41,19 +5,17 @@ def main():
     uploaded_file = st.file_uploader("Choose an image...", type=["jpg", "jpeg", "png"])
 
     if uploaded_file is not None:
-        image = Image.open(uploaded_file)
-        st.image(image, caption="Uploaded Image", use_column_width=True)
+        try:
+            image = Image.open(uploaded_file)
+            st.image(image, caption="Uploaded Image", use_column_width=True)
 
-        
-        
-        # Perform prediction
-        preds = predict_step([uploaded_file])
+            # Perform prediction
+            preds = predict_step([uploaded_file])
 
-        # Display captions
-        st.subheader("Predicted Caption:")
-        for pred in preds:
-            st.write(pred)
+            # Display captions
+            st.subheader("Predicted Caption:")
+            for pred in preds:
+                st.write(pred)
 
-
-if __name__ == "__main__":
-    main()
+        except Exception as e:
+            st.error(f"An error occurred: {e}")
